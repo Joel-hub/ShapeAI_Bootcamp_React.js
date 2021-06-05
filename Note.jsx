3bbf779e510f795de0b30e2402f064e3419eeb87
Joel George <56469327+Joel-hub@users.noreply.github.com>
2021-06-05 16:41:50 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js Bootcamp</h1>
+      <p>
+        Shape AI conducted an illuminating bootcamp on basic web development
+        using Javascript and React.js from 25th to 31st of May 2021. The
+        sessions were taken by Shaurya Sinha, a passionate teacher who loves to
+        teach everything from scratch.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
